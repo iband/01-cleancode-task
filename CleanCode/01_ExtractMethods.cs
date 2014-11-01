349cef8f0df43ce8374dd186c1254e11bde9853f
Vitaly Ishkulov <vit@iva-soft.com>
2014-11-01 17:52:36 +0500

    Refactored 01 task

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -5,26 +5,21 @@ namespace CleanCode
 {
 	public static class RefactorMethod
 	{
+        private static void WriteToFile(string path, byte[] data)
+        {
+            var fs = new FileStream(path, FileMode.OpenOrCreate);
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+
+        }
 		private static void SaveData(string s, byte[] d)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
+            WriteToFile(s, d);
+            // backup
+            WriteToFile(Path.ChangeExtension(s, "bkp"), d);
 
 			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+            WriteToFile(s + ".time", BitConverter.GetBytes(DateTime.Now.Ticks));
 		}
 	}
 }
